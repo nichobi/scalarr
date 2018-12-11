315f4c733e17b1e547f007fb1150eb0130569445
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2018-12-11 23:55:54 +0100

    Add initial code for reading config and sending API requests

--- src/main/scala/scalarr/scalarr.scala
+++ src/main/scala/scalarr/scalarr.scala
@@ -1,7 +1,21 @@
 package scalarr
+import com.typesafe.config.{Config,ConfigFactory}
+import com.softwaremill.sttp._
 
 object scalarr {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val config = ConfigFactory.load("scalarr.conf")
+    val sonarrBase = "http://localhost:8989/api"
+    val keySonarr = config.getString("sonarr.apikey")
+
+    val firstRequest = sttp
+      .get(uri"$sonarrBase/diskspace")
+      .header("X-Api-Key", keySonarr)
+
+    implicit val backend = HttpURLConnectionBackend()
+    val firstResponse = firstRequest.send()
+    
+    // firstResponse: Response[String]
+    println(firstResponse.body)
   }
 }
