9b8a49b97ead20f2c97d1f2a1a17ec7421be372e
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2019-06-17 11:23:58 +0200

    Define function to parse series lookup results

--- src/main/scala/scalarr/sonarr.scala
+++ src/main/scala/scalarr/sonarr.scala
@@ -9,15 +9,24 @@ case class Sonarr(address: String, port: Int, apiKey: String){
   def get(endpoint: String, params: (String, String)*): ujson.Value = {
     val request = sttp.get(base.path(s"api/$endpoint").params(params.toMap))
       .header("X-Api-Key", apiKey)
-     val response = request.send()
+    val response = request.send()
     val parsed = ujson.read(response.unsafeBody)
     parsed
   }
+
   def search(query: String) = {
     val request = sttp
       .get(uri"$base/series/lookup")
       .header("X-Api-Key", apiKey)
     val response = request.send()
     println(response)
+  }
+
+  def formatSeries(seriesList: ujson.Value): Seq[String] = {
+    for(i <- seriesList.arr.indices) yield{
+      val series = seriesList(i)
+      s"""($i) ${series("title").str} - ${series("year").num.toInt}
+     |  ${series("status").str} - Seasons: ${series("seasonCount").num.toInt} """.stripMargin
+    }
   }
 }
