914ec4af8ea1b5deffefb7d0800465487f240fdb
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2019-06-17 11:19:25 +0200

    Add helper function for sending Sonarr requests

--- src/main/scala/scalarr/sonarr.scala
+++ src/main/scala/scalarr/sonarr.scala
@@ -1,9 +1,18 @@
 package scalarr
 import com.softwaremill.sttp._
+import ujson._
 case class Sonarr(address: String, port: Int, apiKey: String){
 
   val base = Uri(address).port(port).path("/api")
   implicit val backend = HttpURLConnectionBackend()
+
+  def get(endpoint: String, params: (String, String)*): ujson.Value = {
+    val request = sttp.get(base.path(s"api/$endpoint").params(params.toMap))
+      .header("X-Api-Key", apiKey)
+     val response = request.send()
+    val parsed = ujson.read(response.unsafeBody)
+    parsed
+  }
   def search(query: String) = {
     val request = sttp
       .get(uri"$base/series/lookup")
