5a1468bee821382a24e2895007e668ec6d64d4df
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2019-10-15 17:42:01 +0200

    Make chooseFrom retry upon failed parsing

--- src/main/scala/util/interactive.scala
+++ src/main/scala/util/interactive.scala
@@ -23,6 +23,30 @@ object interactive {
     chooseFromHelper(map, prompt)
   }
 
+  case class QuitException() extends Exception("Quit") {
+    override def toString = "User quit"
+  }
+
+  def tryToReadUntilQuit[A](options: SortedMap[Int, A], prompt: String)(
+      implicit reader: Reader): Task[A] = {
+    val result: Task[A] = for {
+      quitFansi   <- Task.succeed(fansi.Color.LightBlue("Q").render + "uit")
+      indexString <- reader.readOption(s"Choose $prompt or $quitFansi: ")
+      value <- if (indexString.toLowerCase == "q") Task.fail(QuitException())
+              else attemptToRead(indexString, options)
+    } yield value
+    result.catchSome {
+      case QuitException() => Task.fail(QuitException())
+      case err             => putStrLn(err.toString) *> tryToReadUntilQuit(options, prompt)
+    }
+  }
+
+  def attemptToRead[A](indexString: String, options: SortedMap[Int, A]) =
+    for {
+      index <- Task(indexString.toInt)
+      value <- Task(options(index))
+    } yield value
+
   private def chooseFromHelper[A](
       options: SortedMap[Int, A],
       prompt: String
@@ -42,14 +66,12 @@ object interactive {
       case 1 => Task.succeed(options.head._2)
       case _ =>
         for {
-          _           <- putStrLn(options.show)
-          indexString <- reader.readOption(s"Choose $prompt: ")
-          index       <- Task(indexString.toInt)
-          value       <- Task(options(index))
+          _     <- putStrLn(options.show)
+          value <- tryToReadUntilQuit(options, prompt)
         } yield value
     }
     result.foldM(
-      err => Task.fail(new Exception("Failure picking option: " + err.getMessage, err)),
+      err => Task.fail(new Exception(s"Failure picking $prompt: " + err.toString, err)),
       chosen => putStrLn(s"${prompt.capitalize}: $chosen") *> Task.succeed(chosen)
     )
   }
